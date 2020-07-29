03dff31de88ae38779f3ad0bf776eda74e4e463b
Nelson Chaves <nelsonchavespro@gmail.com>
2020-07-28 21:35:51 -0700

    Pause

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,2 +1,6 @@
 class Bug < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true, length: { maximum: 500 }
+  validates :priority, numericality: { greater_than: 0 }
+  validates :state, inclusion: { in: ['open', 'closed'] }
 end
